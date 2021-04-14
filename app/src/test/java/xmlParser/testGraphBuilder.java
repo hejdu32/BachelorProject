89e6faef350891592a58910ba437c6f622ed855e
Simon Hjerrild Rold Sørensen <201807232@post.au.dk>
2021-04-14 15:16:47 +0200

    Another edit

--- app/src/test/java/xmlParser/testGraphBuilder.java
+++ app/src/test/java/xmlParser/testGraphBuilder.java
@@ -93,6 +93,7 @@ public class testGraphBuilder {
     }
     @Test
     public void testWriteToFileAsJson() throws IOException {
-        graphBuilder.writeToFileAsJson("");
+        String filePath = "";
+        graphBuilder.writeToFileAsJson(filePath);
     }
 }
